f6f9e40ba7e53988d42b637e98296988e60fb160
spyderfalcon <79997661+spyder-falcon@users.noreply.github.com>
2022-01-25 11:29:29 +0530

    main.py

---
+++ RUN.py
@@ -0,0 +1,20 @@
+#For safty to avoid error set a and b =
+
+print("\n\nFind Area of Square,rectangel\n") 
+a=0
+b=0
+#Find The Area of square and rectangle
+a=input("Input Length:")
+b=input ("Input Breadth:")
+c=int(a)*int(b)
+#-------------SPYDER_FALCON----------
+#check the vlaue and find its a square or rectangle
+if a==b:
+    print("This is a Square And Its Area =",c)  # https://instagram.com/spyder_falcon
+else :
+    print("Area =",c)     
+
+
+num = input('\n\nPress Enter To Exit ')
+
+#end    
